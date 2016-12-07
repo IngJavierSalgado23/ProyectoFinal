deb228fe625c14c9facd38d7b7a093fb48331486
IngJavierSalgado23 <javier.salgado@cetys.edu.mx>
2016-12-06 21:41:10 -0800

    Update Funciones.py

--- Funciones.py
+++ Funciones.py
@@ -11,7 +11,6 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 from Tkinter import *
 import matplotlib.pyplot as plt
-from graficaUso import *
 from threading import Thread
 import threading
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
